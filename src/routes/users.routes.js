7f8de0107140416efbeff906750be024a2cc7180
Antoniza <82987222+Antoniza@users.noreply.github.com>
2022-06-29 20:33:17 -0600

    Preparando Lanzanmiento

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,19 +1,35 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const {selectUsersQuery, insertUsersQuery, selectUsersUnique, deleteUsersUnique, updateUsersQuery} = require('../controllers/users.controller');
+const {
+  selectUsersQuery,
+  insertUsersQuery,
+  selectUsersUnique,
+  deleteUsersUnique,
+  updateUsersQuery,
+} = require("../controllers/users.controller");
 
-router.get('/', (req, res) => {
-    res.json("{Message: 'Estas en la tabla de users. La estructura es la siguiente:', username: '', email: '', password: '', number: ''}");
+router.get("/", (req, res) => {
+  const initTableUsers = {
+    Message: "Estas en la tabla de users.",
+    Structure: {
+      Message: 'La estructura es la siguiente',
+      username: "",
+      email: "",
+      password: "",
+      number: ""
+    }
+  };
+  res.json(initTableUsers);
 });
 
-router.get('/actionTableUsers/', selectUsersQuery);
+router.get("/actionTableUsers/", selectUsersQuery);
 
-router.get('/actionTableUsers/:id', selectUsersUnique);
+router.get("/actionTableUsers/:id", selectUsersUnique);
 
-router.post('/actionTableUsers', insertUsersQuery);
+router.post("/actionTableUsers", insertUsersQuery);
 
-router.delete('/actionTableUsers/:id', deleteUsersUnique);
+router.delete("/actionTableUsers/:id", deleteUsersUnique);
 
-router.put('/actionTableUsers/:id', updateUsersQuery);
+router.put("/actionTableUsers/:id", updateUsersQuery);
 
 module.exports = router;
